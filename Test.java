62aab8284a31f4883b54d01d919ea0bdc2592ce3
vuxthe <vux.ptit@gmail.com>
2021-09-19 15:07:14 +0700

    test

---
+++ Test.java
@@ -0,0 +1,12 @@
+/**
+ * Test
+ */
+
+import java.math.BigInteger;
+
+public class Test {
+    public static void main(String[] args) {
+        BigInteger A  = BigInteger.ONE;
+        System.out.println(A);
+    }
+}
