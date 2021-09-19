c1a9f9cb08e4b00335b49fd1a9370e13c6ffaef1
vuxthe <90490826+vuxthe@users.noreply.github.com>
2021-09-19 15:32:53 +0700

    Update Test.java

--- Test.java
+++ Test.java
@@ -6,7 +6,6 @@ import java.math.BigInteger;
 
 public class Test {
     public static void main(String[] args) {
-        BigInteger A  = BigInteger.ONE;
-        System.out.println(A);
+        System.out.println("hello");
     }
 }
